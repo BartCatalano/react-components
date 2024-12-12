43ee35f99d460cba0ab5ec0035f242865392df6a
BartCatalano <catalanobartolomeo@gmail.com>
2024-12-12 16:35:04 +0100

    feat: card fatta. footer (WIP)

--- src/components/appCard.jsx
+++ src/components/appCard.jsx
@@ -7,8 +7,11 @@ function AppCard() {
                 <section className={style.img}>
                     </section>
                 <section className={style.description}>
-                    <div>Titolo</div>
-                    <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure in adipisci iste quam deserunt recusandae assumenda iusto rerum dicta asperiores, quisquam alias perferendis voluptate ducimus corporis dolore totam laboriosam ipsum!</div>
+                    <div className={style.title}><h2>Titolo Del Post</h2></div>
+                    <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                         Iure in adipisci iste quam deserunt recusandae assumenda iusto 
+                         rerum dicta asperiores, quisquam alias perferendis voluptate ducimus
+                          corporis dolore totam laboriosam ipsum!</div>
                 </section>
             </div>
         </div>
