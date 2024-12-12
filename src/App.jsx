c6e3b1fb68e8594c450f10db5822f297d014f022
BartCatalano <catalanobartolomeo@gmail.com>
2024-12-12 16:45:35 +0100

    feat: footer fatto. bottone (WIP)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import AppHeader from "./components/appHeader"
 import AppCard from "./components/appCard"
-
+import AppFooter from "./components/AppFooter"
 function App() {
   
   return (
     <>
  <AppHeader />
  <AppCard />
+<AppFooter />
     </>
   )
 }
